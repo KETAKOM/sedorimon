124f829ff634d94d7424a81c59a607fe99c7d596
kotake_shunki <ketakom2111178@gmail.com>
2020-03-07 23:39:48 +0900

    fix

--- src/alert.js
+++ src/alert.js
@@ -3,8 +3,20 @@
 const rank_up_limit = 40000;
 const three_months_total_sales_limit = 4
 
-window.onload = function() {
+// 「Webアプリケーションとして導入」しているGASのURL
+const gasUrl = "https://script.google.com/macros/s/AKfycbxR8Rf_EaGZ8FIT5q_HT-QshRjwgZPeyO00yas_HAbvwFXvoMtn/exec";
 
+// $(function() {
+//     // 画面にボタンを挿入する
+//     $('.item_box').prepend('<input type="button" id="postBtn" value="リストに保存">');
+
+//     // ボタンがクリックされたときの処理
+//     $('#postBtn').on('click', function() {
+        
+//     });
+// });
+
+window.onload = function() {
     var l = false;
     l = Logic();
 
@@ -37,7 +49,8 @@ function Logic () {
 }
 
 function Acrion() {
-    alert("OK");
+    Save();
+    CloseWindow();
 }
 
 function BadAction() {
@@ -46,4 +59,60 @@ function BadAction() {
 
 function CloseWindow() {
     window.close();
+}
+
+function Save() {
+    var obj = {
+        name: "",
+        mono_url: "",
+        amazon_url: "",
+        asin: "",
+        jan: "",
+        standard_num: "",
+        rank: "",
+    }
+
+    var amazon_detail = $(".product_data_summary_box").prev("div").children("h3").children("a");
+
+    obj.name = amazon_detail.text();
+    obj.mono_url = location.href;
+    obj.amazon_url = amazon_detail.attr("href");
+    obj.asin = $("#_asin").text();
+    obj.jan = $("#export_ean_code_wrap").children(".clip_bd").text();
+    obj.standard_num = $("#export_ean_code_wrap").parent().next("span").children(".clip_bd").text();
+    obj.rank = $(".data_ranking").text();
+
+    post2GAS(obj);
+}
+
+function post2GAS(obj){
+    var data = {
+        name: obj.name,
+        mono_url: obj.mono_url,
+        amazon_url: obj.amazon_url,
+        asin: obj.asin,
+        jan: obj.jan,
+        standard_num: obj.standard_num,
+        rank: obj.rank,
+    }
+
+    console.log(data);
+    $.ajax({
+        type: 'POST',
+        url: gasUrl,
+        data: JSON.stringify(data)
+    })
+    .done( function(data) {
+        console.log(data);
+        console.log("成功");
+    })
+    .fail( function(jqXHR, textStatus, errorThrown) {
+        console.log("失敗");
+        console.log("XMLHttpRequest : " + jqXHR.status);
+        console.log("textStatus : " + textStatus);
+        console.log("errorThrown : " + errorThrown);
+    })
+    .always( (data) => {
+        console.log("処理終了");
+    });
 }
